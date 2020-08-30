57e90a761db90525bbd11460df9d24790f834d04
sr.teppei <jesus.hernanz.mateo@gmail.com>
2020-08-30 20:01:44 +0200

    Generate lagout component in configuration module

--- src/app/configuration/configuration.module.ts
+++ src/app/configuration/configuration.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrainerComponent } from './component/trainer/trainer.component';
 import { ClientComponent } from './component/client/client.component';
+import { LayoutComponent } from './component/layout/layout.component';
 
 
 
 @NgModule({
-  declarations: [TrainerComponent, ClientComponent],
+  declarations: [TrainerComponent, ClientComponent, LayoutComponent],
   imports: [
     CommonModule
   ]
