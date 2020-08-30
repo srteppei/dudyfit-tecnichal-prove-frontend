22073ef657727706b5bc04d53005d919d9f1e831
sr.teppei <jesus.hernanz.mateo@gmail.com>
2020-08-30 19:58:38 +0200

    Create component trainer configuration

--- src/app/configuration/configuration.module.ts
+++ src/app/configuration/configuration.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TrainerComponent } from './component/trainer/trainer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TrainerComponent],
   imports: [
     CommonModule
   ]
