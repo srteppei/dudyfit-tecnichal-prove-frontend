3f636446e69e139ab9d9cd9f02476b427cf1311a
sr.teppei <jesus.hernanz.mateo@gmail.com>
2020-08-30 19:57:49 +0200

    Create module configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ConfigurationModule } from './configuration/configuration.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    ConfigurationModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
