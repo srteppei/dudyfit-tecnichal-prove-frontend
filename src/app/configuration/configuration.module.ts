925356c614e5a0059519aa5739ff8b6d84fa5bd4
sr.teppei <jesus.hernanz.mateo@gmail.com>
2020-08-30 20:30:20 +0200

    Add router configuration

--- src/app/configuration/configuration.module.ts
+++ src/app/configuration/configuration.module.ts
@@ -3,13 +3,31 @@ import { CommonModule } from '@angular/common';
 import { TrainerComponent } from './component/trainer/trainer.component';
 import { ClientComponent } from './component/client/client.component';
 import { LayoutComponent } from './component/layout/layout.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path: 'configuration',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'trainer',
+        component: TrainerComponent,
+      },
+      {
+        path: 'client',
+        component: ClientComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   declarations: [TrainerComponent, ClientComponent, LayoutComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild(routes),
+  ],
+  exports: [RouterModule]
 })
 export class ConfigurationModule { }
