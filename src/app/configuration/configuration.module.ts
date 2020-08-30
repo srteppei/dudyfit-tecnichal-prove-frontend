094f975c42ef9abfabe45531a67d46e59bd40ac6
sr.teppei <jesus.hernanz.mateo@gmail.com>
2020-08-30 20:01:02 +0200

    Generate configuration component client

--- src/app/configuration/configuration.module.ts
+++ src/app/configuration/configuration.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrainerComponent } from './component/trainer/trainer.component';
+import { ClientComponent } from './component/client/client.component';
 
 
 
 @NgModule({
-  declarations: [TrainerComponent],
+  declarations: [TrainerComponent, ClientComponent],
   imports: [
     CommonModule
   ]
