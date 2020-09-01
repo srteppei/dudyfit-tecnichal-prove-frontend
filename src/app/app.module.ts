94ac826cc4a1865bea7ae45502e15faac3d235e9
sr.teppei <jesus.hernanz.mateo@gmail.com>
2020-09-01 21:33:18 +0200

    Create module assignation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DudyFitApiModule } from './dudy-fit-api/dudy-fit-api.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AssignationModule } from './assignation/assignation.module';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     NgbModule,
     ConfigurationModule,
     DudyFitApiModule,
+    AssignationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
