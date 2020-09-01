53b09d205c5a9a9141d7ba8980b2cd63b1389fdb
sr.teppei <jesus.hernanz.mateo@gmail.com>
2020-09-01 21:40:40 +0200

    Configure routes

--- src/app/assignation/assignation.module.ts
+++ src/app/assignation/assignation.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AssignationComponent } from './component/assignation/assignation.component';
+import { Routes, RouterModule } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path: 'assignation',
+    component: AssignationComponent
+  }
+];
 
 @NgModule({
   declarations: [AssignationComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
+  ],
+  exports: [
+    RouterModule,
   ]
 })
 export class AssignationModule { }
